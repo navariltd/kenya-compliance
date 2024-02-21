09fa2f4a66ae646e44561d169983067784647d67
Okeyo Gichana Mayaka <gichanamayaka@gmail.com>
2024-02-21 10:36:05 +0300

    implemented client-side functionality to the integration settings doctype. Reworked the workspace.

--- kenya_compliance/kenya_compliance/doctype/doctype_names_mapping.py
+++ kenya_compliance/kenya_compliance/doctype/doctype_names_mapping.py
@@ -7,6 +7,7 @@ COMMUNICATION_KEYS_DOCTYPE_NAME: Final[str] = "KRA eTims Communication Keys Nava
 SETTINGS_DOCTYPE_NAME: Final[str] = "KRA eTims Settings Navari"
 LAST_REQUEST_DATE_DOCTYPE_NAME: Final[str] = "KRA eTims Last Request Date Navari"
 ROUTES_TABLE_DOCTYPE_NAME: Final[str] = "KRA eTims Routes Table Navari"
+ROUTES_TABLE_CHILD_DOCTYPE_NAME: Final[str] = "KRA eTims Route Child Table"
 
 # Global Variables
 SANDBOX_SERVER_URL: Final[str] = "https://etims-api-sbx.kra.go.ke/etims-api"
