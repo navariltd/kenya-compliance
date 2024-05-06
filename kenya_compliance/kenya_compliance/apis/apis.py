b0257eb1618a116c10790b3127a8c20e23856f63
GichanaMayaka <gichanamayaka@gmail.com>
2024-05-06 14:02:59 +0200

    fix: bug fix

--- kenya_compliance/kenya_compliance/apis/apis.py
+++ kenya_compliance/kenya_compliance/apis/apis.py
@@ -387,9 +387,7 @@ def submit_inventory(request_data: str) -> None:
             endpoints_builder.headers = headers
             endpoints_builder.url = url
             endpoints_builder.payload = payload
-            endpoints_builder.success_callback = partial(
-                inventory_submission_on_success, document_name=data["name"]
-            )
+            endpoints_builder.success_callback = lambda response: frappe.errprint(f"{response}")
             endpoints_builder.error_callback = on_error
 
             frappe.enqueue(
