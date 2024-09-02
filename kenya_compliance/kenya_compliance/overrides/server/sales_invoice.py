dadbb0e869f72ba0e2620ebba7d31a9767a57656
GichanaMayaka <gichanamayaka@gmail.com>
2024-09-02 15:05:17 +0300

    fix: Removed check to filter out consolidated invoices.

--- kenya_compliance/kenya_compliance/overrides/server/sales_invoice.py
+++ kenya_compliance/kenya_compliance/overrides/server/sales_invoice.py
@@ -6,9 +6,5 @@ from .shared_overrides import generic_invoices_on_submit_override
 def on_submit(doc: Document, method: str) -> None:
     """Intercepts submit event for document"""
 
-    if (
-        not doc.is_consolidated
-        and not doc.custom_successfully_submitted
-        and doc.update_stock == 1
-    ):
+    if doc.custom_successfully_submitted == 0 and doc.update_stock == 1:
         generic_invoices_on_submit_override(doc, "Sales Invoice")
